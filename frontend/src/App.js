28bc395156bccf69c17c58c387f6f9dbb92d1e02
erjonarifi <81029500+erjonarifi@users.noreply.github.com>
2026-05-06 21:34:15 +0200

    fix backend url for production

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,16 @@ import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import Navbar from './components/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
-
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Cart from './pages/Cart';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import AdminDashboard from './pages/admin/AdminDashboard';
-
+import axios from 'axios';
 import './App.css';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 function App() {
   return (
